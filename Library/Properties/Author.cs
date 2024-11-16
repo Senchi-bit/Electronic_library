9a62644bb61c4266aa9d19d7178eaacf80f1af1c
Arsen Islyamov <arsenislyam@gmail.com>
2024-11-16 21:31:56 +0300

    Add Author Book Exhibition classes

---
+++ Library/Properties/Author.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.Properties;
+
+public class Author
+{
+    public int id;
+    public string fullName;
+    public Book[] books;
+
+    public Author(int id, string fullName, Book[] books)
+    {
+        this.id = id;
+        this.fullName = fullName;
+        this.books = books;
+    }
+
+}
---
+++ Library/Properties/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.Properties;
+
+public class Book
+{
+    public int id;
+    public string title;
+    public int releasedYear;
+    public Author author;
+
+    public Book(int id, string title, int releasedYear, Author author)
+    {
+        this.id = id;
+        this.title = title;
+        this.releasedYear = releasedYear;
+        this.author = author;
+    }
+}
