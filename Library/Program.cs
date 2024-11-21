1fabdbf5c246931709cbd1c9a139552c168736ca
Arsen Islyamov <arsenislyam@gmail.com>
2024-11-21 18:28:45 +0300

    Update Program.cs

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,6 +1,8 @@
 using Library.Context;
 using Library.Entities;
 using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,107 +10,72 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
-MyDbContext db = new MyDbContext();
-var books = db.Books.ToList();
-var authors = db.Authors.ToList();
-var exhibitions = db.Exhibitions.ToList();
 
+// app.Run((async (context) =>
+// {
+//     var response = context.Response;
+//     var request = context.Request;
+//
+//     else if (path == "/authors/api/author" && request.Method == "GET")
+//     {
+//         await GetAllAuthors(response);
+//     }
+//
+//     else if (path == "/api/exhibitions" && request.Method == "GET")
+//     {
+//         await GetAllExhibitions(response);
+//     }
+//     else if(path == "/authors" && request.Method == "GET")
+//     {
+//         response.ContentType = "text/html; charset=utf-8";
+//         await response.SendFileAsync("html/authors.html");
+//     }
+// }));
 
-
-
-
-app.Run((async (context) =>
+app.MapGet("/api/books",async () =>
 {
-    var response = context.Response;
-    var request = context.Request;
-    var path = request.Path;
-    string regexString = @"^/api/books/\d*$";
-    
-    if (path == "/api/books" && request.Method=="GET")
-    {
-        await GetAllBooks(response); 
-    }
-
-    else if (path == "/authors/api/authors" && request.Method == "GET")
-    {
-        await GetAllAuthors(response);
-    }
-
-    else if (path == "/api/exhibitions" && request.Method == "GET")
-    {
-        await GetAllExhibitions(response);
-    }
-
-    else if (path == "/api/books" && request.Method == "POST")
-    {
-        await AddBook(response, request);
-    }
-    else if (Regex.IsMatch(path, regexString) && request.Method == "GET")
-    {
-        int id = Convert.ToInt32(path.Value?.Split("/")[3]);
-        await GetBook(id, response);
-    }
-    else if(path == "/api/books" && request.Method == "PUT")
-    {
-        await UpdateBook(response, request);
-    }
-    else if (Regex.IsMatch(path, regexString) && request.Method == "DELETE")
-    {
-        int id = Convert.ToInt32(path.Value?.Split("/")[3]);
-        await DeleteBook(id, response);
-    }
-    else if(path == "/authors" && request.Method == "GET")
-    {
-        response.ContentType = "text/html; charset=utf-8";
-        await response.SendFileAsync("html/authors.html");
-    }
+    MyDbContext db = new MyDbContext();
+    return await db.Books.ToListAsync();
+});
+app.MapGet("/api/books/{id}",(int id) =>
+{
+    MyDbContext db = new MyDbContext();
+    var books = db.Books.ToList();
+    var book = books.FirstOrDefault(b => b.Id == id); 
+    if (book != null)
+        return Results.Ok(book);
     else
     {
-        response.ContentType = "text/html; charset=utf-8";
-        await response.SendFileAsync("html/index.html");
+        return Results.NotFound();
     }
-}));
-app.Run();
-
-
-async Task GetAllBooks(HttpResponse response)
-{
-    await response.WriteAsJsonAsync(books);
-}
-async Task GetAllAuthors(HttpResponse response)
-{
-    await response.WriteAsJsonAsync(authors);
-}
-
-async Task GetAllExhibitions(HttpResponse response)
-{
-    await response.WriteAsJsonAsync(exhibitions);
-}
-
-async Task AddBook(HttpResponse response, HttpRequest request)
+});
+app.MapPost("/api/books", async (Book book) =>
 {
     try
     {
-        var lastBook = books.LastOrDefault();
-        var book = await request.ReadFromJsonAsync<Book>();
+        
+        MyDbContext db = new MyDbContext();
+        var books = await db.Books.ToListAsync();
+        var lastBookId = books.Max(x => x.Id);
         if (book != null)
         {
             Book newBook = new Book()
             {
-                Id = lastBook.Id + 1,
+                Id = lastBookId + 1,
                 Title = book.Title,
                 ReleaseYear = book.ReleaseYear
             };
             await db.Books.AddAsync(newBook);
             await db.SaveChangesAsync();
-            await response.WriteAsJsonAsync(newBook);
+            return Results.Ok(newBook);
         }
         else
         {
@@ -117,27 +84,35 @@ async Task AddBook(HttpResponse response, HttpRequest request)
     }
     catch (Exception e)
     {
-        response.StatusCode = 400;
-        await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+        return Results.StatusCode(400);
     }
-}
-
-async Task GetBook(int id,HttpResponse response){
-    var book = books.FirstOrDefault(b => b.Id == id); 
-    if (book != null)
-        await response.WriteAsJsonAsync(book);
-    else
-    {
-        response.StatusCode = 404;
-        await response.WriteAsJsonAsync(new { message = "Книга не найдена" });
-    }
-}
-
-async Task UpdateBook(HttpResponse response, HttpRequest request)
+});
+app.MapPut("/api/books/nameFilter", (Book book) =>
 {
     try
     {
-        var bookData = await request.ReadFromJsonAsync<Book>();
+        MyDbContext db = new MyDbContext();
+        var books = db.Books.Where(b => b.Title.ToLower().Contains(book.Title.ToLower())).ToList();
+        if (books != null)
+        {
+            return Results.Ok(books);
+        }
+        else
+        {
+            throw new Exception("Not found");
+        }
+    }
+    catch (Exception e)
+    {
+        return Results.StatusCode(404);
+    }
+});
+app.MapPut("/api/books",async (Book bookData) =>
+{
+    try
+    {
+        MyDbContext db = new MyDbContext();
+        var books =await db.Books.ToListAsync();
         if (bookData != null)
         {
             var book = books.FirstOrDefault(u => u.Id == bookData.Id);
@@ -147,12 +122,12 @@ async Task UpdateBook(HttpResponse response, HttpRequest request)
                 book.Title = bookData.Title;
                 db.Books.Update(book);
                 await db.SaveChangesAsync();
-                await response.WriteAsJsonAsync(book);
+                return Results.Ok(book);
+                
             }
             else
             {
-                response.StatusCode = 404;
-                await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+                throw new Exception("Книга не найден");
             }
         }
         else
@@ -162,21 +137,21 @@ async Task UpdateBook(HttpResponse response, HttpRequest request)
     }
     catch (Exception)
     {
-        response.StatusCode = 400;
-        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+        return Results.StatusCode(400);
     }
-}
-
-async Task DeleteBook(int id, HttpResponse response)
+});
+app.MapDelete("/api/books/{id}", async (int id) =>
 {
     try
     {
+        MyDbContext db = new MyDbContext();
+        var books = await db.Books.ToListAsync();
         var book = books.FirstOrDefault(b => b.Id == id);
         if (book != null)
         {
             db.Books.Remove(book);
             await db.SaveChangesAsync();
-            await response.WriteAsJsonAsync(book);
+            return Results.Ok(book);
         }
         else
         {
@@ -185,7 +160,18 @@ async Task DeleteBook(int id, HttpResponse response)
     }
     catch (Exception e)
     {
-        response.StatusCode = 404;
-        await response.WriteAsJsonAsync(new { message = e.Message });
+        return Results.StatusCode(404);
     }
+});
+app.Run();
+
+
+/*async Task GetAllAuthors(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(authors);
 }
+
+async Task GetAllExhibitions(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(exhibitions);
+}*/
