2d4128f267ce134e0d4e4f6a69b1f615ef2f0928
Arsen Islyamov <arsenislyam@gmail.com>
2024-11-21 10:02:45 +0300

    Add work books

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,5 +1,6 @@
 using Library.Context;
 using Library.Entities;
+using System.Text.RegularExpressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,52 +12,180 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+MyDbContext db = new MyDbContext();
+var books = db.Books.ToList();
+var authors = db.Authors.ToList();
+var exhibitions = db.Exhibitions.ToList();
+
+
+
+
+
+app.Run((async (context) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-string[] array = new string[3];
-int i = 0;
-using (MyDbContext db = new MyDbContext())
-{
-    var users = db.Books.ToList();
-    Console.WriteLine("Список объектов:");
-    foreach (Book u in users)
+    var response = context.Response;
+    var request = context.Request;
+    var path = request.Path;
+    string regexString = @"^/api/books/\d*$";
+    
+    if (path == "/api/books" && request.Method=="GET")
     {
-        array[i] = ($"{u.Id}.{u.Title} - {u.ReleaseYear}");
-        i++;
+        await GetAllBooks(response); 
+    }
+
+    else if (path == "/authors/api/authors" && request.Method == "GET")
+    {
+        await GetAllAuthors(response);
+    }
+
+    else if (path == "/api/exhibitions" && request.Method == "GET")
+    {
+        await GetAllExhibitions(response);
+    }
+
+    else if (path == "/api/books" && request.Method == "POST")
+    {
+        await AddBook(response, request);
+    }
+    else if (Regex.IsMatch(path, regexString) && request.Method == "GET")
+    {
+        int id = Convert.ToInt32(path.Value?.Split("/")[3]);
+        await GetBook(id, response);
+    }
+    else if(path == "/api/books" && request.Method == "PUT")
+    {
+        await UpdateBook(response, request);
+    }
+    else if (Regex.IsMatch(path, regexString) && request.Method == "DELETE")
+    {
+        int id = Convert.ToInt32(path.Value?.Split("/")[3]);
+        await DeleteBook(id, response);
+    }
+    else if(path == "/authors" && request.Method == "GET")
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/authors.html");
+    }
+    else
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/index.html");
+    }
+}));
+app.Run();
+
+
+async Task GetAllBooks(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(books);
+}
+async Task GetAllAuthors(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(authors);
+}
+
+async Task GetAllExhibitions(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(exhibitions);
+}
+
+async Task AddBook(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        var lastBook = books.LastOrDefault();
+        var book = await request.ReadFromJsonAsync<Book>();
+        if (book != null)
+        {
+            Book newBook = new Book()
+            {
+                Id = lastBook.Id + 1,
+                Title = book.Title,
+                ReleaseYear = book.ReleaseYear
+            };
+            await db.Books.AddAsync(newBook);
+            await db.SaveChangesAsync();
+            await response.WriteAsJsonAsync(newBook);
+        }
+        else
+        {
+            throw new Exception("Некорректные данные");
+        }
+    }
+    catch (Exception e)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
     }
 }
 
-app.MapGet("/weatherforecast", () =>
+async Task GetBook(int id,HttpResponse response){
+    var book = books.FirstOrDefault(b => b.Id == id); 
+    if (book != null)
+        await response.WriteAsJsonAsync(book);
+    else
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-app.MapGet("/", () =>
-{
-    return Results.Ok(array);
-});
-app.Run();
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "Книга не найдена" });
+    }
+}
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+async Task UpdateBook(HttpResponse response, HttpRequest request)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    try
+    {
+        var bookData = await request.ReadFromJsonAsync<Book>();
+        if (bookData != null)
+        {
+            var book = books.FirstOrDefault(u => u.Id == bookData.Id);
+            if (book != null)
+            {
+                book.ReleaseYear = bookData.ReleaseYear;
+                book.Title = bookData.Title;
+                db.Books.Update(book);
+                await db.SaveChangesAsync();
+                await response.WriteAsJsonAsync(book);
+            }
+            else
+            {
+                response.StatusCode = 404;
+                await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+            }
+        }
+        else
+        {
+            throw new Exception("Некорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
+
+async Task DeleteBook(int id, HttpResponse response)
+{
+    try
+    {
+        var book = books.FirstOrDefault(b => b.Id == id);
+        if (book != null)
+        {
+            db.Books.Remove(book);
+            await db.SaveChangesAsync();
+            await response.WriteAsJsonAsync(book);
+        }
+        else
+        {
+            throw new Exception("Not found");
+        }
+    }
+    catch (Exception e)
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = e.Message });
+    }
 }
