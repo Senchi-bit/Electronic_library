bde52a90314184bbc028f6536ef35e9c53207639
Arsen Islyamov <arsenislyam@gmail.com>
2024-12-07 00:34:23 +0300

    add DTO

--- Library/Entities/Author.cs
+++ Library/Entities/Author.cs
@@ -11,3 +11,10 @@ public partial class Author
 
     public virtual ICollection<Book> Books { get; set; } = new List<Book>();
 }
+public partial class AuthorDto
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+    
+}
--- Library/Entities/Book.cs
+++ Library/Entities/Book.cs
@@ -10,6 +10,14 @@ public partial class Book
     public string Title { get; set; } = null!;
 
     public int ReleaseYear { get; set; }
-
     public virtual ICollection<Author> Authors { get; set; } = new List<Author>();
+}
+public partial class BookDto
+{
+
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+
+    public int ReleaseYear { get; set; }
+    public virtual ICollection<AuthorDto> Authors { get; set; }
 }
